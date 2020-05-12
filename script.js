9f5fd37f5fe9c071fdb078a28d572ae036ea84fc
Clara Pasteau <clarapasteau@gmail.com>
2020-05-12 17:27:40 +0200

    ok

--- script.js
+++ script.js
@@ -2,7 +2,48 @@ $(document).ready(function(){
     $(window).on('load', function() {
     $( ".draggable" ).draggable();
     });
-    console.log ('hi no error here yet bye');
+    
+
+    $(".breath").mouseover(function(){
+        $('.breathing-top').hide();
+    });
+    $(".breath").mouseout(function(){
+        $('.breathing-top').show();
+    });
+
+
+//modal here
+
+  var modalButtons = $('.tent');
+  var modals = $('.modal');
+
+  $(modalButtons).on('click', function(e) {
+          toggleModals(e)  
+          console.log('hi')
+  })
+
+function toggleModals(e) {
+  var element = $(e.target);
+  console.log(element)
+	var modalClass = element.attr('data-modal');
+        var modalToOpen = $('.modal-' + modalClass);
+        $(modals).removeClass('open');
+        $(modalToOpen).addClass('open');
+}
+$('.close').on('click', function(){ 
+  $(modals).removeClass('open')
+});
+
 });
 
 
+function PlaySound(mySound) {
+    var thissound=document.getElementById(mySound);
+    thissound.play();
+}
+
+function StopSound(mySound) {
+    var thissound=document.getElementById(mySound);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
